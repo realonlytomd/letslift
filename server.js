4b7574acfecd9995cc8c4f7c8e09c6702459b40c
realonlytomd <mclaughlintomd@gmail.com>
2018-08-11 15:01:05 -0500

    added some user auth files and code into letslift

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+// require passport as configured here
+var passport = require("./config/passport");
 
 var PORT = process.env.PORT || 8080;
 
@@ -15,6 +17,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Need to use sessions to keep track of the user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
