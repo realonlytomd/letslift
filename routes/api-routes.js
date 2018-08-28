41b927ffadcde93f3d95cccc7e3f7ebbe42194e8
realonlytomd <mclaughlintomd@gmail.com>
2018-08-28 15:02:22 -0500

    redirects to members page after w/o input

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -52,8 +52,8 @@ module.exports = function(router) {
       id: req.params.id
     }
   })
-    .then(function(result) {
-      res.json(result);
+    .then(function() {
+      res.redirect(307, "/api/login");
     });
   });
 
