672622b1696595fdffcad5d85c06e65514f65e0e
realonlytomd <mclaughlintomd@gmail.com>
2018-10-12 15:57:48 -0500

    minor comment change

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,8 +22,8 @@ module.exports = function(router) {
   // Otherwise the user will be sent an error
   router.post("/api/login", passport.authenticate("local"), function(req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-    // So we're sending the user back the route to the members page because the redirect will hrouteren on the front end
-    // They won't get this or even be able to access this page if they aren't authed
+    // So we're sending the user back the route to the members page because the redirect will route on the front end
+    // They won't get this or even be able to access this page if they aren't authorized
     res.json("/members");
   });
 
